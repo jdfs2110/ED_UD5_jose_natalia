2b9d7e7303f0a15b885212b670ba2695b6827316
jdfs2110 <jdfsiescamp@gmail.com>
2023-02-22 09:15:36 +0100

    aptitud.java done & calificacion.java started

---
+++ Aptitud.java
@@ -0,0 +1,42 @@
+/** Esta clase define las aptitudes de los estudiantes.
+ * @author jose
+ * @version 22/02/2023
+ */
+public class Aptitud{
+    private String nombreAptitud;
+    private int nivelAptitud;
+
+    /**
+     * método para poner el nombre a la aptitud
+     * @param nombreAptitud el parámetro nombreAptitud define el nombre
+     * de la aptitud.
+     */
+    public void setNombre(String nombreAptitud){
+        this.nombreAptitud = nombreAptitud;
+    }
+
+    /**
+     * método para poner el nivel de la aptitud
+     * @param nivelAptitud el parámetro nombreAptitud define
+     * el nivel de aptitud (numero entero positivo).
+     */
+    public void setNivel(int nivelAptitud){
+        this.nivelAptitud = nivelAptitud;
+    }
+
+    /**
+     * método que devuelve el nombre de la aptitud
+     * @return el nombre de la aptitud
+     */
+    public String getNombre(){
+        return nombreAptitud;
+    }
+
+    /**
+     * método que devuelve el nivel de la aptitud
+     * @return el nivel de la aptitud
+     */
+    public int getNivel(){
+        return nivelAptitud;
+    }
+}
---
+++ Calificacion.java
@@ -0,0 +1,21 @@
+/** */
+public class Calificacion {
+    private String asignatura;
+    private int nota;
+
+    public void setAsignatura(String asignatura){
+        this.asignatura = asignatura;
+    }
+
+    public void setNota(int nota){
+        this.nota = nota;
+    }
+
+    public String getAsignatura(){
+        return asignatura;
+    }
+
+    public int getNota(){
+        return nota;
+    }
+}
