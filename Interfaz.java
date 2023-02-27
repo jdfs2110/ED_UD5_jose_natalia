a6dc99c628087ba3493ee916984ec1f9d3ad7992
jdfs2110 <jdfsiescamp@gmail.com>
2023-02-27 09:20:48 +0100

    added interfaz.java

---
+++ Interfaz.java
@@ -0,0 +1,132 @@
+import java.util.ArrayList;
+/**Clase interfaz
+ * @author jose
+ * @version 27/02/2023
+ */
+public class Interfaz {
+    private ArrayList<OfertaEmpleo> listaOfertas = new ArrayList<OfertaEmpleo>();
+    private ArrayList<Empresa> listaEmpresas = new ArrayList<Empresa>();
+    private ArrayList<Estudiante> listaEstudiantes = new ArrayList<Estudiante>();
+    private ArrayList<Docente> listaDocentes = new ArrayList<Docente>();
+    private ArrayList<Aptitud> listaAptitudes = new ArrayList<Aptitud>();
+
+    /**
+     * método para añadir una oferta de empleo a la lista de ofertas de empleo.
+     * @param args el parámetro args define los argumentos de la oferta de empleo.
+     * @return true si se ha añadido correctamente, false si no se ha añadido.
+     */
+    public boolean addOferta(String[] args){
+    }
+
+    /**
+     * método para eliminar una oferta de empleo de la lista de ofertas de empleo.
+     * @param args el parámetro args define los argumentos de la oferta de empleo.
+     * @return true si se ha eliminado correctamente, false si no se ha eliminado.
+     */
+    public boolean removeOferta(String[] args){
+    }
+
+    /**
+     * método para obtener una oferta de empleo de la lista de ofertas de empleo.
+     * @param args el parámetro args define los argumentos de la oferta de empleo.
+     * @return la oferta de empleo.
+     */
+    public Oferta getOferta(String[] args){
+    }
+
+    /**
+     * método para añadir una empresa a la lista de empresas.
+     * @param args el parámetro args define los argumentos de la empresa.
+     * @return true si se ha añadido correctamente, false si no se ha añadido.
+     */
+    public boolean addEmpresa(String[] args){
+    }
+
+    /**
+     * método para eliminar una empresa de la lista de empresas.
+     * @param args el parámetro args define los argumentos de la empresa.
+     * @return true si se ha eliminado correctamente, false si no se ha eliminado.
+     */
+    public boolean removeEmpresa(String[] args){
+    }
+
+    /**
+     * método para obtener una empresa de la lista de empresas.
+     * @param args el parámetro args define los argumentos de la empresa.
+     * @return la empresa.
+     */
+    public Empresa getEmpresa(String[] args){
+    }
+
+    /**
+     * método para añadir un estudiante a la lista de estudiantes.
+     * @param args el parámetro args define los argumentos del estudiante.
+     * @return true si se ha añadido correctamente, false si no se ha añadido.
+     */
+    public boolean addEstudiante(String[] args){
+    }
+
+    /**
+     * método para eliminar un estudiante de la lista de estudiantes.
+     * @param args el parámetro args define los argumentos del estudiante.
+     * @return true si se ha eliminado correctamente, false si no se ha eliminado.
+     */
+    public boolean removeEstudiante(String[] args){
+    }
+
+    /**
+     * método para obtener un estudiante de la lista de estudiantes.
+     * @param args el parámetro args define los argumentos del estudiante.
+     * @return el estudiante.
+     */
+    public Estudiante getEstudiante(String[] args){
+    }
+
+    /**
+     * método para añadir un docente a la lista de docentes.
+     * @param args el parámetro args define los argumentos del docente.
+     * @return true si se ha añadido correctamente, false si no se ha añadido.
+     */
+    public boolean addDocente(String[] args){
+    }
+
+    /**
+     * método para eliminar un docente de la lista de docentes.
+     * @param args el parámetro args define los argumentos del docente.
+     * @return true si se ha eliminado correctamente, false si no se ha eliminado.
+     */
+    public boolean removeDocente(String[] args){
+    }
+
+    /**
+     * método para obtener un docente de la lista de docentes.
+     * @param args el parámetro args define los argumentos del docente.
+     * @return el docente.
+     */
+    public Docente getDocente(String[] args){
+    }
+
+    /**
+     * método para añadir una aptitud a la lista de aptitudes.
+     * @param args el parámetro args define los argumentos de la aptitud.
+     * @return true si se ha añadido correctamente, false si no se ha añadido.
+     */
+    public boolean addAptitud(String[] args){
+    }
+
+    /**
+     * método para eliminar una aptitud de la lista de aptitudes.
+     * @param args el parámetro args define los argumentos de la aptitud.
+     * @return true si se ha eliminado correctamente, false si no se ha eliminado.
+     */
+    public boolean removeAptitud(String[] args){
+    }
+
+    /**
+     * método para obtener una aptitud de la lista de aptitudes.
+     * @param args el parámetro args define los argumentos de la aptitud.
+     * @return la aptitud.
+     */
+    public Aptitud getAptitud(String[] args){
+    }
+}
