5aa6c4d9ab5927d2014619dabf998528dc78b452
jdfs2110 <jdfsiescamp@gmail.com>
2023-03-01 08:42:11 +0100

    Aptitud.java still not working with javadoc :D

--- Aptitud.java
+++ Aptitud.java
@@ -1,7 +1,50 @@
 /**
  * @author jose
+ * @version 01/03/2023
  */
 public class Aptitud {
+    private String nombreAptitud;
+    private int nivelAptitud;
 
-    
+    /**
+     * Constructor de la clase Aptitud
+     * @param nombreAptitud
+     * @param nivelAptitud
+     */
+    public Aptitud(String nombreAptitud, int nivelAptitud) {
+        this.nombreAptitud = nombreAptitud;
+        this.nivelAptitud = nivelAptitud;
+    }
+
+    /**
+     * Metodo para modificar el nombre de la aptitud
+     * @param nombreAptitud nombre de la aptitud
+     */
+    public void setNombreAptitud(String nombreAptitud) {
+        this.nombreAptitud = nombreAptitud;
+    }
+
+    /**
+     * Metodo que devuelve el nivel de la aptitud
+     * @return
+     */
+    public String getNombreAptitud() {
+        return nombreAptitud;
+    }
+
+    /**
+     * Metodo para modificar el nivel de la aptitud
+     * @param nivelAptitud nivel de la aptitud
+     */
+    public void setNivelAptitud(int nivelAptitud) {
+        this.nivelAptitud = nivelAptitud;
+    }
+
+    /**
+     * Metodo que devuelve el nivel de la aptitud
+     * @return
+     */
+    public int getNivelAptitud() {
+        return nivelAptitud;
+    }
 }
